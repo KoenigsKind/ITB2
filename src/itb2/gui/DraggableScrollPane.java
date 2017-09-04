fd567f6f0094dde6afadbd391cab0f1860bf100f
KoenigsKind <git@koenigskind.net>
2017-09-04 15:52:01 +0200

    Possible Solution with global Mouse Listener as local don't get fired

--- src/itb2/gui/DraggableScrollPane.java
+++ src/itb2/gui/DraggableScrollPane.java
@@ -1,15 +1,18 @@
 package itb2.gui;
 
+import java.awt.AWTEvent;
 import java.awt.Component;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 public class DraggableScrollPane extends JScrollPane {
 	private static final long serialVersionUID = 7574613713592839284L;
+	private final int dragMask;
 	private int mouseX, mouseY;
 	
 	public DraggableScrollPane(Component comp) {
@@ -19,37 +22,49 @@ public class DraggableScrollPane extends JScrollPane {
 	public DraggableScrollPane(Component comp, int dragMask) {
 		super(comp);
 		
+		this.dragMask = dragMask;
+		
 		setWheelScrollingEnabled(false);
-		addMouseListener(new MouseListener() {
-			@Override public void mouseReleased(MouseEvent e) {}
-			@Override public void mouseExited(MouseEvent e) {}
-			@Override public void mouseEntered(MouseEvent e) {}
-			@Override public void mouseClicked(MouseEvent e) {}
+		
+		Toolkit.getDefaultToolkit().addAWTEventListener(new AWTEventListener() {
 			
 			@Override
-			public void mousePressed(MouseEvent e) {
-				System.out.println("Pressed");
-				mouseX = e.getX();
-				mouseY = e.getY();
-			}
-		});
-		addMouseMotionListener(new MouseMotionListener() {
-			@Override public void mouseMoved(MouseEvent e) {}
-			
-			@Override public void mouseDragged(MouseEvent e) {
-				if((e.getModifiersEx() & dragMask) == dragMask) {
-					int dX = mouseX - e.getX();
-					int dY = mouseY - e.getY();
+			public void eventDispatched(AWTEvent event) {
+				if(event instanceof MouseEvent) {
+					MouseEvent mouseEvent = (MouseEvent) event;
 					
-					Rectangle rect = getViewport().getVisibleRect();
-					rect.x += dX;
-					rect.y += dY;
-					getViewport().scrollRectToVisible(rect);
+					Component comp = mouseEvent.getComponent();
+					if(!SwingUtilities.isDescendingFrom(comp, DraggableScrollPane.this))
+						return;
 					
-					mouseX = e.getX();
-					mouseY = e.getY();
+					if(mouseEvent.getID() == MouseEvent.MOUSE_PRESSED)
+						handleMousePressed(mouseEvent);
+					
+					if(mouseEvent.getID() == MouseEvent.MOUSE_DRAGGED)
+						handleMouseDragged(mouseEvent);
 				}
 			}
-		});
+			
+		}, AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_MOTION_EVENT_MASK);
+	}
+	
+	private void handleMousePressed(MouseEvent e) {
+		mouseX = e.getX();
+		mouseY = e.getY();
+	}
+	
+	private void handleMouseDragged(MouseEvent e) {
+		if((e.getModifiersEx() & dragMask) == dragMask) {
+			int dX = mouseX - e.getX();
+			int dY = mouseY - e.getY();
+			
+			Rectangle rect = getViewport().getVisibleRect();
+			rect.x += dX;
+			rect.y += dY;
+			getViewport().scrollRectToVisible(rect);
+			
+			mouseX = e.getX();
+			mouseY = e.getY();
+		}
 	}
 }
