d9ba470f3454fc33a7bb2143e34d364b0cdfbd3e
KoenigsKind <git@koenigskind.net>
2017-12-31 12:56:49 +0100

    Hid BinaryImage and DrawableImage behind interfaces, added them to byte factory (TortoiseGit making some problems...)

---
+++ src/itb2/image/BinaryImage.java
@@ -0,0 +1,13 @@
+package itb2.image;
+
+/**
+ * Binary image, storing only 0 or 1 per pixel
+ *
+ * @author Micha Strauch
+ */
+public interface BinaryImage extends Image {
+	
+	/** ID for channel */
+	public static final int BINARY = 0;
+	
+}
